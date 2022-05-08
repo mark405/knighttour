8cdbe0489683266e4b81ea18b26e19ecc318d4b7
aletiaa <104795940+aletiaa@users.noreply.github.com>
2022-05-09 02:51:28 +0300

    Update Printer.h

--- Printer.h
+++ Printer.h
@@ -9,6 +9,7 @@ public:
 	void printTable(std::vector<std::vector<int>>& table); //printing matrix
 	void printResultErrorMessage(int row, int col);  
 	void printResultSuccessMessage(int row, int col);
+	void printDeadEndMessage(int row, int col);
 	void printIncorrectInputMessage();   
 };
 
