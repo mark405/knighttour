b1d8bb644fd94e07156127b4ccc5e746158fd32a
aletiaa <104795940+aletiaa@users.noreply.github.com>
2022-05-09 02:54:12 +0300

    Update ClosedTourAlgorithm.cpp

--- ClosedTourAlgorithm.cpp
+++ ClosedTourAlgorithm.cpp
@@ -1,30 +1,39 @@
 #include "ClosedTourAlgorithm.h"
 #include "MovesCounter.h"
 
-ClosedTourAlgorithm::ClosedTourAlgorithm(Printer *printer, Move *move)
+ClosedTourAlgorithm::ClosedTourAlgorithm(Printer& printer, Move& move)
 {
     _printer = printer;  
     _move = move;          
 }
 
-bool ClosedTourAlgorithm::findClosedTour(int& rowStart, int& colStart)
+STATUS ClosedTourAlgorithm::findClosedTour(int& rowStart, int& colStart)
 {
     std::vector<std::vector<int>> table(N, std::vector<int>(N, 0)); //matrix with zero values
+    std::vector<std::vector<int>> possibleOptions
+    {
+            {-2, 1,  2, 2,  -1, -1, -2, 1},
+            {-1, -2, 1, -1, 2,  -2, 1,  2}
+    };
+
 
     int row = rowStart, col = colStart;
 
     table[rowStart][colStart] = MovesCounter::IncreaseCount(); //put value of 1 to start position
 
-    while (MovesCounter::Getcount() != 64) 
+
+    while (MovesCounter::Getcount() != 64)
     {
-        if (!_move->nextMove(table, row, col)) //if got lost or didnt find
-        { 
-            return false;
+        if (!_move.nextMove(table, row, col, possibleOptions)) //if got lost or didnt find    
+        {
+            _printer.printTable(table);
+            return TRAP; 
+            //            return false;
         }
     }
 
-    _printer->printTable(table);
+    _printer.printTable(table);
 
-    return _move->neighbour(rowStart, colStart, row, col); //if neighbour
+    return _move.neighbour(rowStart, colStart, row, col, possibleOptions) ? SUCCESS : FAILURE; //if neighbour
 }
 
