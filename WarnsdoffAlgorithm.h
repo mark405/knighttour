b994ee2a459d01644fe150c769aeb3b1c77d3c88
aletiaa <104795940+aletiaa@users.noreply.github.com>
2022-05-09 03:03:27 +0300

    бебра2.0

--- WarnsdoffAlgorithm.h
+++ WarnsdoffAlgorithm.h
@@ -5,16 +5,12 @@
 
 class WarnsdoffAlgorithm
 {
-private:
-	const std::vector<std::vector<int>> possibleOptions
-	{
-			{1, 1,  2, 2,  -1, -1, -2, -2},
-			{2, -2, 1, -1, 2,  -2, 1,  -1}
-	};
 public: 
 	void optimizeByWarnsdoffAlgorithm(
 		std::vector<std::vector<int>>& table, int& index,
 		int& minCount, int rowStart,
-		int colStart); //finding move that has minimum next moves	
+		int colStart,
+		std::vector<std::vector<int>> possibleOptions); //finding move that has minimum next moves	
 };
+
 
