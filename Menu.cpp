9a9c68ef26e812f11938b60e4b62bc50a7a05721
aletiaa <104795940+aletiaa@users.noreply.github.com>
2022-05-14 00:04:36 +0300

    Update Menu.cpp
    added enters -_-

--- Menu.cpp
+++ Menu.cpp
@@ -25,16 +25,22 @@ void Menu::EnterPositions(int& row, char& CharRow, int& col)
     {
         std::cout << "\nRow: ";
         std::cin >> CharRow;
-        if (std::cin.fail()) {
+        
+        if (std::cin.fail()) 
+        {
           std::cin.clear();
           std::cin.ignore(32767, '\n');
         }
+        
         std::cout << "Col: ";  
         std::cin >> col;
-        if (std::cin.fail()) {
+        
+        if (std::cin.fail()) 
+        {
           std::cin.clear();
           std::cin.ignore(32767, '\n');
         }
+        
         CharRow = toupper(CharRow);
         row = CharRow - '0' - 16;
     } while (row <= 0 || col <= 0 || row > N || col > N);
