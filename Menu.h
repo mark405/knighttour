931b01def55a49ee9043fe5d8eec5c8c107bd40c
NyafNyaf <lafnef@gmail.com>
2022-05-15 23:02:43 +0300

    comments for menu and printer

--- Menu.h
+++ Menu.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "Move.h"
 
-class Menu
+class Menu//class that has all methods related to the menu
 {
 public:
-	void PrintOptions();
-	int ChooseDestiny();
-	void EnterPositions(int &row, int &col, char &colChar);
+	void PrintOptions();//printing menu
+	int ChooseDestiny();//choosing action
+	void EnterPositions(int& row, int& col);  //entering starting positions
 };
--- Printer.h
+++ Printer.h
@@ -3,13 +3,13 @@
 #include <vector>
 #include <string>
 
-class Printer
+class Printer//class that has all methods related to printing table and messages
 {
 public:
 	void printTable(std::vector<std::vector<int>>& table); //printing matrix
-	void printResultErrorMessage(char col, int row);
-	void printResultSuccessMessage(char col, int row);
-    void printDeadEndMessage(char col, int row);
-	void printIncorrectInputMessage();
+	void printResultErrorMessage(int row, int col);//printing error message
+	void printResultSuccessMessage(int row, int col);//printing success message
+	void printDeadEndMessage(int row, int col);//printing message when got in trap
+	void printIncorrectInputMessage();//printing message when user chooses incorrect menu item
 };
 
