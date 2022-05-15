3803b5ba8b9a8398ebe6d97beac1b3ea89ecfccc
mark405 <62206298+mark405@users.noreply.github.com>
2022-05-15 23:33:44 +0300

    fixed parameters

--- Menu.h
+++ Menu.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "Move.h"
 
-class Menu//class that has all methods related to the menu
+class Menu
 {
 public:
-	void PrintOptions();//printing menu
-	int ChooseDestiny();//choosing action
-	void EnterPositions(int& row, char& CharRow, int& col);  //entering starting positions
+	void PrintOptions();
+	int ChooseDestiny();
+	void EnterPositions(int &row, int &col, char &colChar);
 };
