4b3cee42bba8a329cf31338f6155f10c69a59e9b
mark405 <62206298+mark405@users.noreply.github.com>
2022-05-16 00:37:13 +0300

    added const references

--- Move.h
+++ Move.h
@@ -7,14 +7,14 @@
 class Move //class that has all methods related to the movement of a knight
 {
 public:
-	bool neighbour(int rowStart, int colStart, int rowEnd, int colEnd, std::vector<std::vector<int>> possibleOptions); //checking if first move is close to last one
+	bool neighbour(int rowStart, int colStart, int rowEnd, int colEnd, const std::vector<std::vector<int>> possibleOptions); //checking if first move is close to last one
 	bool isValid(std::vector<std::vector<int>>& table, int row, int col); //checking if moving knight is possible on position row, col
-	bool nextMove(std::vector<std::vector<int>>& table, int& row, int& col, std::vector<std::vector<int>> possibleOptions); // moving knight
+	bool nextMove(std::vector<std::vector<int>>& table, int& row, int& col, const std::vector<std::vector<int>> possibleOptions); // moving knight
 	void optimizeByWarnsdoffAlgorithm(
 		std::vector<std::vector<int>>& table, int& index,
 		int& minCount, int rowStart,
 		int colStart,
-		std::vector<std::vector<int>> possibleOptions); //finding move that has minimum next moves	
+		const std::vector<std::vector<int>> possibleOptions); //finding move that has minimum next moves	
 };
 
 
