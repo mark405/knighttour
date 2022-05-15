f08d1709ff19acc8006870e370361571c2a97c28
mark405 <62206298+mark405@users.noreply.github.com>
2022-05-16 00:03:52 +0300

    changed arguments

--- Printer.h
+++ Printer.h
@@ -7,9 +7,9 @@ class Printer//class that has all methods related to printing table and messages
 {
 public:
 	void printTable(std::vector<std::vector<int>>& table); //printing matrix
-	void printResultErrorMessage(int row, int col);//printing error message
-	void printResultSuccessMessage(int row, int col);//printing success message
-	void printDeadEndMessage(int row, int col);//printing message when got in trap
+	void printResultErrorMessage(char col, int row);//printing error message
+	void printResultSuccessMessage(char col, int row);//printing success message
+	void printDeadEndMessage(char col, int row);//printing message when got in trap
 	void printIncorrectInputMessage();//printing message when user chooses incorrect menu item
 };
 
